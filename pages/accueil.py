a33118c9815e4e01e580b9325deb2e8ca7ec4d39
Nyv <79833594+supernyv@users.noreply.github.com>
2022-08-28 20:53:55 +0100

    Create accueil.py

---
+++ pages/accueil.py
@@ -0,0 +1,9 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import dash_bootstrap_components as dbc
+
+text = "Ecoglobal Africa Services"
+
+layout = html.Div([
+    html.H1(text,),
+    html.P("Tableau de Bord en Construction...")],
+                  style={"textAlign":"center"})
